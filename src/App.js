fc5df7cbc618e51c6b8f19edda62e1621c784cd8
Ben Bauer <ben.bauer1@gmail.com>
2021-06-07 13:27:18 -0500

    creating UI, setting state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [guessHistory, setGuessHistory] = useState({
+    guess1: "",
+    guess2: "",
+    guess3: "",
+    guess4: "",
+    guess5: "",
+    guess6: "",
+    guess7: "",
+    guess8: "",
+    guess9: ""
+  })
+  const guesses = () => {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="main">
+        <h2>Mastermind!</h2>
+        <div className="g10"></div>
+        <div className="g9"></div>
+        <div className="g8"></div>
+        <div className="g7"></div>
+        <div className="g6"></div>
+        <div className="g5"></div>
+        <div className="g4"></div>
+        <div className="g3"></div>
+        <div className="g2"></div>
+        <div className="g1"></div>
+
+      <div className="guess">
+        <span id="dot1" className="dot"></span>
+        <span className="dot"></span>
+        <span className="dot"></span>
+        <span className="dot"></span>
+        <button>Submit</button>
+      </div>
+      </div>
       </header>
     </div>
   );
