a0e907414509ffad5aa230cdff5b32216e47bf28
Ben Bauer <ben.bauer1@gmail.com>
2021-06-07 13:52:48 -0500

    creating guess logic

--- src/App.js
+++ src/App.js
@@ -3,19 +3,59 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [guessHistory, setGuessHistory] = useState({
-    guess1: "",
-    guess2: "",
-    guess3: "",
-    guess4: "",
-    guess5: "",
-    guess6: "",
-    guess7: "",
-    guess8: "",
-    guess9: ""
-  })
+  const [guessHistory, setGuessHistory] = useState({})
+  const [guessCount, setGuessCount] = useState(0)
+  const [loc1, setLoc1] = useState(0)
+  const [loc2, setLoc2] = useState(0)
+  const [loc3, setLoc3] = useState(0)
+  const [loc4, setLoc4] = useState(0)
   const guesses = () => {
+    return(
+      <>
+      <div className="g10"></div>
+      <div className="g9"></div>
+      <div className="g8"></div>
+      <div className="g7"></div>
+      <div className="g6"></div>
+      <div className="g5"></div>
+      <div className="g4"></div>
+      <div className="g3"></div>
+      <div className="g2"></div>
+      <div className="g1"></div>
+      </>
+    )
+  }
+  const addGuess = () =>{
 
+  }
+
+  const cycleColor = (location) => {
+    let currentColor = ""
+    for(let i = 1; i < 5; i++){
+      switch(i){
+        case 1:
+          curentColor = loc1
+          break;
+        case 2: 
+          currentColor = loc2
+          break;
+        case 3:
+          currentColor = loc3
+          break;
+        case 4:
+          currentColor = loc4
+          break;
+      }
+    }
+  }
+
+  const currentGuess = () =>{
+    return(<>
+          <span onClick={cycleColor(1)} className="dot"></span>
+\         <span onClick={cycleColor(2)} className="dot"></span>
+          <span onClick={cycleColor(3)} className="dot"></span>
+          <span onClick={cycleColor(4)} className="dot"></span>
+        </>)
   }
 
   return (
@@ -23,23 +63,11 @@ function App() {
       <header className="App-header">
       <div className="main">
         <h2>Mastermind!</h2>
-        <div className="g10"></div>
-        <div className="g9"></div>
-        <div className="g8"></div>
-        <div className="g7"></div>
-        <div className="g6"></div>
-        <div className="g5"></div>
-        <div className="g4"></div>
-        <div className="g3"></div>
-        <div className="g2"></div>
-        <div className="g1"></div>
+        {guesses()}
 
       <div className="guess">
-        <span id="dot1" className="dot"></span>
-        <span className="dot"></span>
-        <span className="dot"></span>
-        <span className="dot"></span>
-        <button>Submit</button>
+        {currentGuess()}
+        <button onClick={addGuess()}>Submit</button>
       </div>
       </div>
       </header>
